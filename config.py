8e555b1b767ad903a4da9a20f2e1681cd40fe76d
pride02uok <97547310+pride02uok@users.noreply.github.com>
2022-11-12 13:52:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,16 +8,16 @@ load_dotenv()
 que = {}
 admins = {}
 
-API_ID = int(getenv("API_ID", "4110592"))
-API_HASH = getenv("API_HASH", "aa7c849566922168031b95212860ede0")
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_USERNAME = getenv("OWNER_USERNAME", "ShiningOff")
-BOT_USERNAME = getenv("BOT_USERNAME")
+API_ID = int(getenv("API_ID", "26502256"))
+API_HASH = getenv("API_HASH", "c27a809ec73636d95ef93d2e5fb2b7c4")
+BOT_TOKEN = 5342984295:AAEtEwAkX1j-YUQgg9hTbDsCMnWBOrmeQ_I
+OWNER_USERNAME = getenv("pri_de02", "ShiningOff")
+BOT_USERNAME = getenv("ppridemusicbot")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "SilentVerse")
 BOT_NAME = getenv("BOT_NAME","sɪʟᴇɴᴛ ᴍᴜsɪᴄ ʀᴏʙᴏᴛ🥀")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "360"))
-SESSION_NAME = getenv("SESSION_NAME", None)
+SESSION_NAME = AgGUZHAAaroNDbKkezKYwNXbswwJkLJGQKpvL6OTnjHlL8OC6tMXtwrMQH4HjDvfOimtXlU-IpW9Yl__3SySZBQh_I9ggMNRwtQLn4Ydb143JpQfPs5h2yZnVX7hSaIPzspMHfF_2YJhljXOpI7mYLtJzi6OF-UYCvdXFIlJ5O018loKFSInMOaOGLlqwXBnAqOcWyGZxgCm9Mtr8zDG8yIinJT_3dtSE6fvJS-0jgBo8wvi3dnB1qbmM56Ug55h3msQCWWIknRWBMJfLPEqnFVOuIc3B-_4DZnV4UL4GFO2PclYhutfUwpFlk_Ju7t9_A6WxCcKjJPYjpi68xhj6mshO5iU7gAAAAFPoM8bAA
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 PMPERMIT = getenv("PMPERMIT", "DISABLE")
 BOT_IMG = getenv("BOT_IMG", "https://telegra.ph/file/2c3097ae03f950800a66f.jpg")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5280801259").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5630906139").split()))
